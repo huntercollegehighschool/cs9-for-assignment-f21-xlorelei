1434f1fe2e0b8be9cf2664c1c067e826d46f6f0d
Tiffany Xue <93930518+xlorelei@users.noreply.github.com>
2021-12-21 19:26:09 +0000

    Part 2 Defined an initial sum as 0 before changing the for function to add the cube of each number up to 'number.'

--- part2.py
+++ part2.py
@@ -8,3 +8,12 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+
+
+number = int(input("Enter a number: "))
+
+sum = 0
+
+for i in range(1, number+1):
+  sum = sum + i**3
+print(sum)
